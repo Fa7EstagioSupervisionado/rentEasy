051b3240d148d09c5c74d374573cce50a48409a4
Diego Farias <diego.farias@fortaleza.ce.gov.br>
2014-10-31 18:50:06 -0300

    dashboard locadora, ajustes em telas e permissoes

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -10,5 +10,5 @@ class Veiculo < ActiveRecord::Base
 	validates_presence_of :placa, :marca, :modelo, :ano, :cor, :valor_diaria, :garagem_id, :categoria_id, :locadora_id
 
 	has_attached_file :foto, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :foto, :content_type => /\Aimage\/.*\Z/
+  	validates_attachment_content_type :foto, :content_type => /\Aimage\/.*\Z/
 end
